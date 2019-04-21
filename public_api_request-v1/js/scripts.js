c35485867bd88ac496bbce5f9da887df002c701a
Douglas Hill <dohill06@gmail.com>
2019-04-20 18:53:24 -0700

    Clean up

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -13,40 +13,18 @@ function employeeCard(employees) {
     employees.forEach(card => {
         employeeArray.push(card)
 
-        // let employee = `
-        // <div class="card">
-        //     <div class="card-img-container">
-        //         <img class="card-img" src="${card.picture.large}" alt="profile picture">
-        //     </div>
-        //     <div class="card-info-container">
-        //         <h3 id="name" class="card-name cap">${card.name.first} ${card.name.last}</h3>
-        //         <p class="card-text">${card.email}</p>
-        //         <p class="card-text cap">${card.location.city}</p>
-        //     </div>
-        // </div>
-
-        // <div class="modal-container" style="display:none">
-        //     <div class="modal">
-        //         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        //         <div class="modal-info-container">
-        //             <img class="modal-img" src="${card.picture.large}" alt="profile picture">
-        //             <h3 id="name" class="modal-name cap">${card.name.first} ${card.name.last}</h3>
-        //             <p class="modal-text">${card.email}</p>
-        //             <p class="modal-text cap">${card.location.city}</p>
-        //             <hr>
-        //             <p class="modal-text">${card.phone}</p>
-        //             <p class="modal-text cap">${card.location.street}, ${card.location.state}, ${card.location.postcode}</p>
-        //             <p class="modal-text">Birthday: ${card.dob.date}</p>
-        //         </div>
-        //     </div>
-
-        //     <div class="modal-btn-container">
-        //         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-        //         <button type="button" id="modal-next" class="modal-next btn">Next</button>
-        //     </div>
-        // </div>
-        // `
-        gallery.innerHTML += employeeDiv(card.picture.large, card.name.first, card.name.last, card.email, card.location.city, card.phone, card.location.street, card.location.state, card.location.postcode, card.dob.date);
+        gallery.innerHTML += employeeDiv(
+                                card.picture.large,
+                                card.name.first,
+                                card.name.last,
+                                card.email,
+                                card.location.city,
+                                card.phone,
+                                card.location.street,
+                                card.location.state, 
+                                card.location.postcode, 
+                                card.dob.date
+                            );
 
     })
     console.log(employeeArray);
@@ -99,7 +77,7 @@ function employeeDiv(pic, firstName, lastName, email, city, phone, street, state
                     <button type="button" id="modal-next" class="modal-next btn">Next</button>
                 </div>
             </div>
-            `
+            `;
 
     return employee;
 }
